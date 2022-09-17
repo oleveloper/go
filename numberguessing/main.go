65a97ad866d424e776e860439680622d12f1f36c
oleveloper <oleveloper@gmail.com>
2022-09-17 22:59:41 +0900

    Received numeric value from user

--- numberguessing/main.go
+++ numberguessing/main.go
@@ -4,11 +4,42 @@ import (
 	"fmt"
 	"time"
 	"math/rand"
+	"bufio"
+	"os"
 )
+
+var stdin = bufio.NewReader(os.Stdin)
+
+func InputIntValue() (int, error) {
+	var n int
+	_, err := fmt.Scanln(&n)
+	if err != nil {
+		stdin.ReadString('\n')
+	}
+	return n, err
+}
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	n := rand.Intn(100)
-	fmt.Println(n)
+	r := rand.Intn(100)
+	cnt := 1
+
+	for {
+		fmt.Println("Enter an integer value")
+		n, err := InputIntValue()
+		if err != nil {
+			fmt.Println("Please enter an integer only")
+		} else {
+			if n > r {
+				fmt.Println("The number entered is greater than the answer")
+			} else if n < r{
+				fmt.Println("The number entered is less than the answer")
+			} else {
+				fmt.Println("You got the numbers right! Congratulations! Number of attempts:", cnt)
+				break
+			}
+			cnt++
+		}
+	}
 }
